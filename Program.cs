1c61b1092399bfb1e98fa758044fae9f30d18d50
ABHI <abhishekitte11@gmail.com>
2021-09-06 21:51:33 +0530

    [ABHISHEK] Added a snake and ladder

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SnakeAndLadder.cs
+{
+    class SnakeLadder
+    {
+        public static void Position()
+        {
+            int position = 0;
+            Console.WriteLine("Player is at start position:" + position);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Single player");
+            SnakeLadder.Position();
+            Console.ReadLine();
+        }
+    }
+}
